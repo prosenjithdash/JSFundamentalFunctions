781ebe748893186b81dd54fc32f2bc5e7dbd192d
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-03 17:07:34 +0600

    Arguments done

---
+++ Arguments.js
@@ -0,0 +1,12 @@
+// Arguments
+
+function difarence(num1, num2) {
+    result = num1 - num2;
+    console.log(result);
+}
+
+const dif1 = 50;
+const dif2 = 30;
+
+// dif1 & dif2 is Arguments
+difarence(dif1, dif2);
