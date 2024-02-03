394c27f3568992c9b2aeb7698fc50c07d01692b4
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-03 16:41:26 +0600

    Stared

---
+++ Function.js
@@ -0,0 +1,29 @@
+//Functions
+
+//Syntex
+
+// keyword functionsName (){
+
+//}
+
+
+// Function declear
+function name() {
+    console.log('Prosenjith Dash Pappu')
+    console.log('prosenjith.p.w@gmail.com')
+}
+
+
+// Call the functions
+name();
+
+//What is the difarence of loop and functions
+//Ans => loop is run continueously but function not like that if you want to console middle part anythink it is possible.
+
+console.log(':::----------:::')
+console.log(':::----------:::')
+console.log(':::----------:::')
+
+name();
+
+
